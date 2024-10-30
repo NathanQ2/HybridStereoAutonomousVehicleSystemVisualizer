a93b9aa177b7ee662adcdfeb21655a5ccdd3c6cc
Nathan Q <Natethegreat08q@gmail.com>
2024-10-29 21:17:40 -0400

    it works

--- scripts/Controller.cs
+++ scripts/Controller.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 
 public partial class Controller : Node
@@ -10,9 +11,13 @@ public partial class Controller : Node
 	private Socket m_Socket;
 	private Task m_ConnectTask;
 
+	public Label m_DebugLabel;
+
 	// Called when the node enters the scene tree for the first time.
 	public override async void _Ready()
 	{
+		m_DebugLabel = GetNode<Label>("DebugLabel");
+
 		m_Socket = new Socket(
 			m_IpEndpoint.AddressFamily,
 			SocketType.Stream,
@@ -21,21 +26,23 @@ public partial class Controller : Node
 
 		// TODO: Make this async better
 		GD.Print("Waiting for connection...");
-		m_ConnectTask = m_Socket.ConnectAsync(m_IpEndpoint);
-		GD.Print("Connected.");
-
-		var test = new NetworkFrame();
-		
+		m_DebugLabel.Text = "Status: Waiting for connection\n";
+		await m_Socket.ConnectAsync(m_IpEndpoint);
+		m_DebugLabel.Text += "Connected!\n";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override async void _Process(double delta)
 	{
-		if(m_ConnectTask.IsCompleted) {
-			byte[] buff = new byte[2];
-			await m_Socket.ReceiveAsync(buff, SocketFlags.None);
-			int i = BitConverter.ToInt16(buff);
-			GD.Print($"Result: {i}");
-		}
+		byte[] buff = new byte[4];
+		await m_Socket.ReceiveAsync(buff, SocketFlags.None);
+		int i = BitConverter.ToUInt16(buff);
+
+		StringBuilder hex = new StringBuilder(buff.Length * 2);
+		foreach (byte b in buff)
+			hex.AppendFormat("{0:x2}", b);
+
+		m_DebugLabel.Text = $"Result: {i}\nHex: {hex}";
+		GD.Print($"Result: {i} Hex: {hex}");
 	}
 }
